a04577977f2a90d383fcf529d9278b2fb8154050
dandrok <patryk.stusik@gmail.com>
2021-05-16 18:51:13 +0200

    fetch data from firebase

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import pen from './KroneApollo-e1571972772455.png';
-import classes from './Product.module.css';
+import React from 'react'
+// import pen from './KroneApollo-e1571972772455.png';
+import classes from './Product.module.css'
 
-const Product = () => {
+const Product = (props) => {
   return (
     <div className={classes.product}>
-      <img src={pen} alt="pen" className={classes.product__img} />
-      <h4 className={classes.product__title}>Nice Pen</h4>
-      <h3>39.99 EUR</h3>
+      <img
+        src={props.image}
+        alt={props.name}
+        className={classes.product__img}
+      />
+      <h4 className={classes.product__title}>{props.name}</h4>
+      <h3>{props.price}</h3>
     </div>
-  );
-};
+  )
+}
 
-export default Product;
+export default Product
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,41 +1,65 @@
-import classes from './Products.module.css';
+import classes from './Products.module.css'
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react'
 
-import Product from './Product';
-import Titles from './Titles';
+import Product from './Product'
+import Titles from './Titles'
 
 const Products = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true)
+  const [loadedProduct, setLoadedProduct] = useState([])
 
-  fetch(
-    'https://products-react-project-default-rtdb.europe-west1.firebasedatabase.app/products.json'
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-    });
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(
+      'https://products-react-project-default-rtdb.europe-west1.firebasedatabase.app/products.json'
+    )
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        const products = []
+
+        for (const key in data) {
+          const product = {
+            id: key,
+            ...data[key],
+          }
+
+          products.push(product)
+        }
+
+        setIsLoading(false)
+        setLoadedProduct(products)
+      })
+  }, [])
 
   if (isLoading) {
-    return;
+    return (
+      <section>
+        <p>Loading ...</p>
+      </section>
+    )
   }
 
   return (
     <div className={classes.products}>
       <Titles />
       <section className={classes.section}>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {loadedProduct.map((prd) => (
+          <Product
+            key={prd.id}
+            id={prd.id}
+            color={prd.color}
+            description={prd.description}
+            name={prd.name}
+            price={prd.price}
+            image={prd.image}
+          />
+        ))}
       </section>
     </div>
-  );
-};
+  )
+}
 
-export default Products;
+export default Products
